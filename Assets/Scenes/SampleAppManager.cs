1cfb828abb2299b852c85d91ef580c9ccbc2de9e
Tongchun Kim <dejavuwing@gmail.com>
2020-04-23 14:25:12 +0900

    First Commit to master
    First Commit to master

---
+++ Assets/Scenes/SampleAppManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleAppManager : MonoBehaviour
+{
+    public Text HelloText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnHelloButton()
+    {
+        HelloText.GetComponent<Text>().text = "Hello Tongchun!";
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
